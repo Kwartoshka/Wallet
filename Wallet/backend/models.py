88aca713b1bae9e9751b1ada56a5045098c15987
kwartoshka <kwartoshka@gmail.com>
2022-03-11 19:49:24 +0300

    models v1 added

--- Wallet/backend/models.py
+++ Wallet/backend/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+
+class Account(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    balance = models.DecimalField(max_digits=12, decimal_places=2, validators=[
+        MinValueValidator(0.01),
+        MaxValueValidator(10 ** 9)])
+
+    def __str__(self):
+        return f'{self.title} by {self.user} with balance:{self.balance}'
+
+class Operation(models.Model):
+    operation_from = models.ManyToManyField(Account, on_delete=models.CASCADE)
+    operation_to = models.ForeignKey(Account, on_delete=models.CASCADE)
+    sum = models.DecimalField(max_digits=12, decimal_places=2, validators=[
+        MinValueValidator(0.01),
+        MaxValueValidator(10 ** 9)])
+
+    def __str__(self):
+        return f'Operation from{self.operation_from} to {self.operation_to}. Sum:{self.sum}'
+
