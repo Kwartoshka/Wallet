1502bb40f888c1dac5ca7f9d8ddfcfe04521fcf7
kwartoshka <kwartoshka@gmail.com>
2022-03-12 10:12:40 +0300

    operation validation added

--- Wallet/backend/serializers.py
+++ Wallet/backend/serializers.py
@@ -57,3 +57,23 @@ class ShortOperationSerializer(serializers.ModelSerializer):
         account.update(balance=balance)
 
         return super().create(validated_data)
+
+    def validate(self, data):
+        method = self.context["request"].method
+        user = self.context["request"].user
+        operation_from = data['operation_from']
+        operation_to = data['operation_to']
+        separated_sum = data['sum'] / len(operation_from)
+        for account in operation_from:
+            if account.user != user:
+                raise serializers.ValidationError(f'Account {account.id} with name {account.title}'
+                                                  f' does not belong to you!')
+            elif account.balance < separated_sum:
+                raise serializers.ValidationError(f'Account {account.id} with name {account.title}'
+                                                  f' does not have enough money! Please, check your balance.')
+            else:
+                pass
+        if operation_to in operation_from:
+            raise serializers.ValidationError(f'You can not send money to the same account ({operation_to.title}).')
+        else:
+            return data
